b8c1b0ed52997ab2b24de374e0eb565e7f2c0ec2
lumijoe <spacedesign63@gmail.com>
2024-02-13 15:59:56 +0900

    add views/2files

---
+++ src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="about">
+      <h1>This is AboutView</h1>
+      <p>Learn more about us!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AboutView',
+  }
+  </script>
+  
+  <style scoped>
+  /* ここにスタイルを追加 */
+  </style>
+  
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="home">
+      <h1>This is HomeView</h1>
+      <p>Welcome to our home page!</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeView',
+  }
+  </script>
+  
+  <style scoped>
+  /* ここにスタイルを追加 */
+  </style>
+  
