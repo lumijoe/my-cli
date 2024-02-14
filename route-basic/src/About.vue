8a930ca36bfb1799e75ba7551e7a38273578db36
lumijoe <spacedesign63@gmail.com>
2024-02-14 14:28:42 +0900

    add About.vue

---
+++ route-basic/src/About.vue
@@ -0,0 +1,17 @@
+<!-- About.vue  -->
+<template>
+    <div class="about">
+        <h1>This is an about page</h1>
+        <button v-on:click="onclick">トップへ</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            onclick() {
+                this.$router.push('/')
+            }
+        }
+    }
+</script>
