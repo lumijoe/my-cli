65fb6a54d47b4abfc5bf5449e01dd65e52e84406
lumijoe <spacedesign63@gmail.com>
2024-02-16 16:28:14 +0900

    fix eslint

---
+++ route-auth/src/components/Signin.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="Signin">
+      <h3>ログインページ（ダミー）</h3>
+      <div>
+        初回だけ、このページが表示され、自動的に認証が行われます。<br />
+        2回目以降は、直接Aboutページに移動します。<br />
+        ブラウザーを閉じて、再度アクセスすると、このページが表示されます。
+      </div>
+    </div>
+  </template>
+  <!-- eslint-disable-next-line -->
+  <script>
+  export default {
+    // eslint-disable-next-line
+    name: 'signin',
+    created() {
+      // 認証済であることを記録（ダミー）
+      sessionStorage['authed'] = 1;
+    }
+  }
+  </script>
---
+++ route-auth/src/views/About.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable-next-line -->
+<template>
+    <div class="about">
+      <h1>This is an about page</h1>
+    </div>
+  </template>
