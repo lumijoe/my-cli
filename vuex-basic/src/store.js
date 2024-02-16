1645738274d2d7abf97b613183b5a10e3f936f86
lumijoe <spacedesign63@gmail.com>
2024-02-16 17:49:48 +0900

    page348

---
+++ vuex-basic/src/store.js
@@ -0,0 +1,18 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+  strict: true,
+  state: {
+    count: 0
+  },
+  mutations: {
+    minus(state) {
+      state.count--;
+    },
+    plus(state) {
+      state.count++;
+    }
+  },
+  actions: {}
+});
+
