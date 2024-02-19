b1a18f4629e8dd237e9a90ca435281df9a1a12f8
lumijoe <spacedesign63@gmail.com>
2024-02-19 14:24:26 +0900

    change text vuex-basic App

--- vuex-basic/src/App.vue
+++ vuex-basic/src/App.vue
@@ -1,30 +1,54 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <input type="button" value="-" v-on:click="minus" />    
+    {{count}}
+    <input type="button" value="+" v-on:click="plus" />
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+//import { mapMutations } from 'vuex'
+
+export default {
+  name: 'app',
+  /*
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  */
+
+  computed: mapState(['count']),
+
+  /*
+  computed: {
+    ...mapState([ 'count' ]),
+    ...mapState({ countNumber: 'count' })
+  },
+  */
+
+  methods: {
+    minus() {
+      this.$store.commit('minus')
+    },
+    plus() {
+      this.$store.commit('plus')
+    }
+  }
+
+  //methods: mapMutations([ 'plus', 'minus' ])
+}
+</script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
