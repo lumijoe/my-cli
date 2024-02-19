e34c1cbcdb06a09b69ba64d66284d578ac96da12
lumijoe <spacedesign63@gmail.com>
2024-02-19 16:04:07 +0900

    test jest

--- tests-unit/tests/unit/example.spec.js
+++ tests-unit/tests/unit/example.spec.js
@@ -5,8 +5,12 @@ describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
     const wrapper = shallowMount(HelloWorld, {
-      props: { msg }
+      propsData: { msg }
     })
     expect(wrapper.text()).toMatch(msg)
-  })
-})
+
+    const new_msg = 'こんにちは、Vue!!'
+    wrapper.setProps({ msg: new_msg })
+    expect(wrapper.find('h1').text()).toBe(new_msg)
+   })
+ })
